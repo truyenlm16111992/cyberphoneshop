91944348214c7afda3776a2df14b9f3b6ed85f9c
Le Minh Truyen <mtruyenvl@gmail.com>
2023-07-08 17:11:55 +0700

    update source

---
+++ controller/index.js
@@ -0,0 +1,32 @@
+// import { getElements } from "../utils/utils.js";
+// import { Carousel } from 'flowbite';
+// const items=[];
+// getElements("#slider div[data-carousel-item]").forEach((el,position)=>{
+//     items.push({position,el});
+// });
+// const indicators=[];
+// getElements("#slider div[data-carousel-item]").forEach((el,position)=>{
+//     indicators.push({position,el});
+// });
+// const options = {
+//     defaultPosition: 1,
+//     interval: 3000,
+//     indicators: {
+//         activeClasses: 'bg-lime-500 dark:bg-gray-800',
+//         inactiveClasses: 'bg-white/50 dark:bg-gray-800/50 hover:bg-white dark:hover:bg-gray-800',
+//         items: indicators
+//     },
+//     // callback functions
+//     onNext: () => {
+//         console.log('next slider item is shown');
+//     },
+//     onPrev: ( ) => {
+//         console.log('previous slider item is shown');
+//     },
+//     onChange: ( ) => {
+//         console.log('new slider item has been shown');
+//     }
+// };
+// const carousel = new Carousel(items, options);
+
+
---
+++ utils/utils.js
@@ -0,0 +1,3 @@
+const getElements = (_selector) => document.querySelectorAll(_selector);
+const test=1;
+export {getElements, test};
